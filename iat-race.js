223f6f94108d61c9617bd142b474586a99af1399
J-laird49 <jordyndlaird@gmail.com>
2024-11-18 21:39:24 +0000

    Update iat-race.js to include pos and neg words

--- iat-race.js
+++ iat-race.js
@@ -1,6 +1,36 @@
 define(['pipAPI', 'https://cdn.jsdelivr.net/gh/J-laird49/iat-study@main/iat-core.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
+    //Randomly select which of two sets of category labels to use.
+    let raceSet = API.shuffle(['a','b'])[0];
+    let blackLabels = [];
+    let whiteLabels = [];
+
+    if (raceSet == 'a') {
+        blackLabels.push('African Americans');
+        whiteLabels.push('European Americans');
+    } else {
+        blackLabels.push('Black people');
+        whiteLabels.push('White people');
+    }
+
+    API.addGlobal({
+        raceiat:{},
+        raceSet:raceSet,
+        blackLabels:blackLabels,
+        whiteLabels:whiteLabels,
+        //Select randomly what attribute words to see. 
+        //Based on Axt, Feng, & Bar-Anan (2021).
+        posWords : API.shuffle([
+            'Glad', 'Joyful', 'Delight', 'Friendship', 'Terrific', 'Happy', 'Friend', 'Adore', 'Beautiful'
+        ]), 
+        negWords : API.shuffle([
+            'Horrible', 'Humiliate', 'Hurtful', 'Bothersome', 'Disgust', 'Awful', 'Rotten', 'Poison', 'Abuse'
+        ])
+    });
+
+    let global = API.getGlobal();
+	
 	return iatExtension({
 		category1 : {
 			name : 'Black people', //Will appear in the data.
@@ -36,10 +66,50 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/J-laird49/iat-study@main/iat-core
     			{image : 'wm6_nc.jpg'}			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
-
-		base_url : {//Where are your images at?
-			image : 'https://github.com/J-laird49/iat-study/tree/main/images'
-		} 
-	});
+		},
+        attribute1 : {
+            name : 'Bad words',
+            title : {
+                media : {word : 'Bad words'},
+                css : {color:'#0000FF','font-size':'1.8em'},
+                height : 4 //Used to position the "Or" in the combined block.
+            },
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: global.negWords[0]},
+                {word: global.negWords[1]},
+                {word: global.negWords[2]},
+                {word: global.negWords[3]},
+                {word: global.negWords[4]},
+                {word: global.negWords[5]},
+                {word: global.negWords[6]},
+                {word: global.negWords[7]}
+            ],
+            //Stimulus css
+            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+        },
+        attribute2 : {
+            name : 'Good words',
+            title : {
+                media : {word : 'Good words'},
+                css : {color:'#0000FF','font-size':'1.8em'},
+                height : 4 //Used to position the "Or" in the combined block.
+            },
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: global.posWords[0]},
+                {word: global.posWords[1]},
+                {word: global.posWords[2]},
+                {word: global.posWords[3]},
+                {word: global.posWords[4]},
+                {word: global.posWords[5]},
+                {word: global.posWords[6]},
+                {word: global.posWords[7]}
+            ],
+            //Stimulus css
+            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+	},
+	base_url : {//Where are your images at?
+		image : 'https://github.com/J-laird49/iat-study/tree/main/images'
+	},
+	isTouch : global.$isTouch
+     });
 });
