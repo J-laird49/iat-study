dfc0dcbd13c78edea4f9918eb4d2bfcbce61f805
J-laird49 <jordyndlaird@gmail.com>
2024-12-13 15:42:47 +0000

    Update iat-race.js ex update

--- iat-race.js
+++ iat-race.js
@@ -35,37 +35,38 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/J-laird49/iat-study@main/iat-core
 		category1 : {
 			name : global.blackLabels, //Will appear in the data.
 			title : {
-				media : {word : 'Black people'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				media : {word : global.blackLabels}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'bf14_nc.jpg'}, 
-    			{image : 'bf23_nc.jpg'}, 
-    			{image : 'bf56_nc.jpg'}, 
-    			{image : 'bm14_nc.jpg'}, 
-    			{image : 'bm23_nc.jpg'}, 
-    			{image : 'bm56_nc.jpg'}
+    				{image : 'bf14_nc.jpg'}, 
+    				{image : 'bf23_nc.jpg'}, 
+    				{image : 'bf56_nc.jpg'}, 
+    				{image : 'bm14_nc.jpg'}, 
+    				{image : 'bm23_nc.jpg'}, 
+    				{image : 'bm56_nc.jpg'}
 			], 
 			//Stimulus css (style)
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+			stimulusCss : {color:'#31940F','font-size':'2.3em'}
 		},	
-		category2 :	{
-			name : gloabl.whiteLabels, //Will appear in the data.
+		category2 : {
+			name : global.whiteLabels, //Will appear in the data.
 			title : {
-				media : {word : 'White people'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				media : {word : global.whiteLabels}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'wf2_nc.jpg'}, 
-    			{image : 'wf3_nc.jpg'}, 
-    			{image : 'wf6_nc.jpg'}, 
-    			{image : 'wm1_nc.jpg'}, 
-    			{image : 'wm4_nc.jpg'}, 
-    			{image : 'wm6_nc.jpg'}			], 
-			//Stimulus css
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+    		    		{image : 'wf2_nc.jpg'}, 
+    				{image : 'wf3_nc.jpg'}, 
+    				{image : 'wf6_nc.jpg'}, 
+    				{image : 'wm1_nc.jpg'}, 
+    				{image : 'wm4_nc.jpg'}, 
+    				{image : 'wm6_nc.jpg'}			
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'2.3em'}
 		},
         attribute1 : {
             name : 'Bad words',
